6f6ec856dfbba724b338294a462aff546a75d11a
Jason <na>
2025-05-22 10:37:28 -0400

    Added a dbcontext and initial entities.

---
+++ Data/Entities/VideoEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoScripts.Data.Entities;
+
+public class VideoEntity
+{
+    [ForeignKey("Project")]
+    public Guid? ProjectId { get; set; }
+    [Required]
+    public string YTId { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    [ForeignKey("Channel")]
+    public Guid ChannelId { get; set; }
+    public int ViewCount { get; set; }
+    public int LikeCount { get; set; }
+    public int CommentCount { get; set; }
+    public int Duration { get; set; }
+    public DateTime PublishedAt { get; set; }
+    public string? RawTranscript
+    {
+        get; set;
+    public string? Summary { get; set; }
+    // Navigation properties
+    public virtual ProjectEntity Project { get; set; }
+    public virtual ChannelEntity Channel { get; set; }
+    public virtual ICollection<TranscriptTopicEntity> TranscriptTopics { get; set; } = new List<TranscriptTopicEntity>();
+
+}
