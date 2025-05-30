dad8fac80729e0ba9d0f8a874c5144a69480592d
Jason <na>
2025-05-29 23:41:31 -0400

    Added feature folder and prompt for creating a script.

--- Features/AnalyzeClusters/prompts.cs
+++ Features/AnalyzeClusters/prompts.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using VideoScripts.Data.Common;
-
-namespace VideoScripts.Features.AnalyzeClusters;
+﻿namespace VideoScripts.Features.AnalyzeClusters;
 
 public static class Prompts
 {
